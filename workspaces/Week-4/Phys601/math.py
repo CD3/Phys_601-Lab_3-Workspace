7e1a900a7bdda326b0ae0636d35f38dbcecfb23e
CD Clark III <clifton.clark@gmail.com>
2025-05-01 10:07:24 -0500

    added week 4

---
+++ workspaces/Week-4/Phys601/math.py
@@ -0,0 +1,36 @@
+import math
+import textwrap
+import time
+
+delay = 0.1
+
+pi = math.pi
+
+for f in [
+    "exp",
+    "sin",
+    "cos",
+    "tan",
+    "tan2",
+    "asin",
+    "acos",
+    "atan",
+    "atan2",
+    "erf",
+    "erfc",
+    "factorial",
+    "log",
+    "log10",
+    "sqrt",
+    "sinh",
+    "cosh",
+    "tanh",
+    "perm",
+    "comb",
+]:
+    code = f"""\
+    def {f}(*args):
+        time.sleep(delay)
+        return math.{f}(*args)
+    """
+    exec(textwrap.dedent(code))
